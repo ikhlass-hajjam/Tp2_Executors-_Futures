bb52d575c2d50e6817e29853b31d0deaf32bc70a
Rémi Bastide <Remi.Bastide@gmail.com>
2020-11-22 19:47:36 +0100

    Simplifié CompteurDeCaracteres

--- src/main/java/futures/CompteurDeCaracteres.java
+++ src/main/java/futures/CompteurDeCaracteres.java
@@ -1,12 +1,11 @@
 package futures;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 import java.net.URL;
 import java.time.Duration;
 import java.util.concurrent.Callable;
 import java.time.Instant;
+import java.util.Scanner;
 
 public class CompteurDeCaracteres implements Callable<ResultatDuCompte> {
 
@@ -18,7 +17,6 @@ public class CompteurDeCaracteres implements Callable<ResultatDuCompte> {
 
     /**
      * Lit le contenu d'une URL, calcule le nombre d'octets et le temps écoulé
-     * Décrémente le compte à rebours
      * @return ResultatDuCompte, contient le nombre d'octets et le temps écoulé
      * @throws IOException en cas d'erreur de lecture
      */
@@ -26,20 +24,10 @@ public class CompteurDeCaracteres implements Callable<ResultatDuCompte> {
     public ResultatDuCompte call() throws IOException {
         Instant start = Instant.now();
 
-        int nombreDeCaracteres = 0;
-        URL url = new URL(urlATraiter);
-
-        // On lit le contenu de l'URL ligne par ligne
-        try (LineNumberReader in =
-                 new LineNumberReader(
-                     new InputStreamReader(url.openStream())
-                 )
-        ) {
-            String line;
-            while ((line = in.readLine()) != null) {
-                nombreDeCaracteres += line.length();
-            }
-        }
+        // Formule magique pour lire le contenu d'un URL dans une chaine de caractères
+        String out = new Scanner(new URL(urlATraiter).openStream(), "UTF-8").useDelimiter("\\A").next();
+        
+        int nombreDeCaracteres = out.length();
 
         System.out.printf("L'URL %s compte %d octets %n", urlATraiter, nombreDeCaracteres);
         Duration tempsDeCalcul = Duration.between(start, Instant.now());
